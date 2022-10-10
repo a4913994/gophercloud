74f407a5e94edd6f255e0f1976e2f3d3679efb64
刘宪东 <liuxiandong10@gmail.com>
2022-10-10 13:42:37 +0800

    feather: add migrate host params

--- openstack/compute/v2/extensions/migrate/requests.go
+++ openstack/compute/v2/extensions/migrate/requests.go
@@ -5,9 +5,33 @@ import (
 	"github.com/lxdcc/gophercloud/openstack/compute/v2/extensions"
 )
 
+// MigrateOptsBuilder allows extensions to add additional parameters to the
+// Migrate request.
+type MigrateOptsBuilder interface {
+	ToMigrateMap() (map[string]interface{}, error)
+}
+
+// MigrateOpts specifies parameters of live migrate action.
+type MigrateOpts struct {
+	// The host to which to migrate the server.
+	// If this parameter is None, the scheduler chooses a host.
+	// Migrate Server (migrate Action) (v2.56)
+	Host *string `json:"host"`
+}
+
+// ToMigrateMap constructs a request body from MigrateOpts.
+func (opts LiveMigrateOpts) ToMigrateMap() (map[string]interface{}, error) {
+	return gophercloud.BuildRequestBody(opts, "migrate")
+}
+
 // Migrate will initiate a migration of the instance to another host.
-func Migrate(client *gophercloud.ServiceClient, id string) (r MigrateResult) {
-	resp, err := client.Post(extensions.ActionURL(client, id), map[string]interface{}{"migrate": nil}, nil, nil)
+func Migrate(client *gophercloud.ServiceClient, id string, opts MigrateOptsBuilder) (r MigrateResult) {
+	b, err := opts.ToMigrateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	resp, err := client.Post(extensions.ActionURL(client, id), b, nil, nil)
 	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
 	return
 }
